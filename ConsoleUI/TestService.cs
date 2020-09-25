737710a471b0943c069afe311372c33d54f37cca
Christoph Hilmer <christoph.hilmer@st.oth-regensburg.de>
2020-09-25 18:19:57 +0200

    Add project files.

---
+++ ConsoleUI/TestService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleUI
+{
+    public class TestService : ITestService
+    {
+        private readonly ILogger<TestService> _log;
+        private readonly IConfiguration _config;
+
+        public TestService(ILogger<TestService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+            {
+                _log.LogInformation("Run num {runNumber}", i);
+            }
+        }
+    }
+}
